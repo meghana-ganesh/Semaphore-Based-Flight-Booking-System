6f911676456de9595f1c6b80aff6dd6b528c2696
meghana <ganeshmeghana2@gmail.com>
2023-10-21 17:44:21 +0530

    Semaphore Solution Implemented in Python

--- Semaphore_Solution.py
+++ Semaphore_Solution.py
@@ -1,7 +1,10 @@
-booking_type = 1 #0:normal 1:rebooking 2:cancellation
+import threading
+
+x_semaphore = threading.Semaphore(4)
+
+booking_type = 2  # 0:normal 1:rebooking 2:cancellation
 
 MRP1 = 10000
-x = 9 #x:rating
 MRP2 = 0
 refund = 0
 
@@ -9,57 +12,145 @@ refund = 0
 def collect_MRP1():
     print(f"Transaction completed with amount {MRP1}")
 
+
 def call_add_page():
     print("THIS IS AN ADVERTISMENT/SURVEY")
-    #add page
+    # add page
+
 
 def rebooking_overhead():
     global MRP2
-    MRP2 - 0.1*MRP1
-    print(f"Booking price:{MRP1}")
-    print(f"Additional rebooking overhead:{MRP2}")
-    print(f"Please pay total amount:{MRP1+MRP2}")
-    #transaction page
+    MRP2 = 0.1 * MRP1
+    print(f"Booking price: {MRP1}")
+    print(f"Additional rebooking overhead: {MRP2}")
+    print(f"Please pay total amount: {MRP1 + MRP2}")
+    # transaction page
+
 
 if __name__ == "__main__":
     if booking_type == 0:
-        if x == 10:
+        if x_semaphore._value == 10:  # Check if the semaphore count is 0.
             collect_MRP1()
         else:
-            x = x + 1
-            print(f"rating increased to:{x}")
+            x_semaphore.release()  # Release the semaphore.
+            print(f"rating increased to: {x_semaphore._value}")
             collect_MRP1()
     elif booking_type == 1:
-        if x == 10:
-            x = x - 1
-            print(f"rating decreased to:{x}")
+        if x_semaphore._value == 10:  # Check if the semaphore count is 10.
+            x_semaphore.acquire()
+            print(f"rating decreased to: {x_semaphore._value}")
             collect_MRP1()
         else:
-            if x < 5 and x > 0:
+            if x_semaphore._value < 5 and x_semaphore._value > 0:  # Check the semaphore count.
                 call_add_page()
-                x = x - 1
-                print(f"rating decreased to:{x}")
+                x_semaphore.acquire()
+                print(f"rating decreased to: {x_semaphore._value}")
                 rebooking_overhead()
             else:
                 call_add_page()
-                x = x - 1
-                print(f"rating decreased to:{x}")
-                print(f"Total amount to be paid:{MRP1}")
+                x_semaphore.acquire()
+                print(f"rating decreased to: {x_semaphore._value}")
+                print(f"Total amount to be paid: {MRP1}")
     else:
-        if x == 10:
-            x = x - 1
-            print(f"rating decreased to:{x}")
-            refund = MRP1 - 0.1*MRP1
+        if x_semaphore._value == 10:  # Check if the semaphore count is 10.
+            x_semaphore.acquire()
+            print(f"rating decreased to: {x_semaphore._value}")
+            refund = MRP1 - 0.1 * MRP1
+            print(f"Amount refunded : {refund}")            
         else:
-            if x < 5 and x > 0:
+            if x_semaphore._value < 5 and x_semaphore._value > 0:  # Check the semaphore count.
                 call_add_page()
-                x = x - 1
-                print(f"rating decreased to:{x}")
-                refund = MRP1 - 0.2*MRP1
+                x_semaphore.acquire()
+                print(f"rating decreased to: {x_semaphore._value}")
+                refund = MRP1 - 0.2 * MRP1
+                print(f"Amount refunded : {refund}")
             else:
                 call_add_page()
-                x = x - 1
-                refund = MRP1 - 0.3*MRP1
+                x_semaphore.acquire()
+                refund = MRP1 - 0.3 * MRP1
+                print(f"Amount refunded : {refund}")
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#algorithm:
+
+# booking_type = 1 #0:normal 1:rebooking 2:cancellation
+
+# MRP1 = 10000
+# x = 9 #x:rating
+# MRP2 = 0
+# refund = 0
+
+
+# def collect_MRP1():
+#     print(f"Transaction completed with amount {MRP1}")
+
+# def call_add_page():
+#     print("THIS IS AN ADVERTISMENT/SURVEY")
+#     #add page
+
+# def rebooking_overhead():
+#     global MRP2
+#     MRP2 = 0.1*MRP1
+#     print(f"Booking price:{MRP1}")
+#     print(f"Additional rebooking overhead:{MRP2}")
+#     print(f"Please pay total amount:{MRP1+MRP2}")
+#     #transaction page
+
+# if __name__ == "__main__":
+#     if booking_type == 0:
+#         if x == 10:
+#             collect_MRP1()
+#         else:
+#             x = x + 1
+#             print(f"rating increased to:{x}")
+#             collect_MRP1()
+#     elif booking_type == 1:
+#         if x == 10:
+#             x = x - 1
+#             print(f"rating decreased to:{x}")
+#             collect_MRP1()
+#         else:
+#             if x < 5 and x > 0:
+#                 call_add_page()
+#                 x = x - 1
+#                 print(f"rating decreased to:{x}")
+#                 rebooking_overhead()
+#             else:
+#                 call_add_page()
+#                 x = x - 1
+#                 print(f"rating decreased to:{x}")
+#                 print(f"Total amount to be paid:{MRP1}")
+#     else:
+#         if x == 10:
+#             x = x - 1
+#             print(f"rating decreased to:{x}")
+#             refund = MRP1 - 0.1*MRP1
+#         else:
+#             if x < 5 and x > 0:
+#                 call_add_page()
+#                 x = x - 1
+#                 print(f"rating decreased to:{x}")
+#                 refund = MRP1 - 0.2*MRP1
+#             else:
+#                 call_add_page()
+#                 x = x - 1
+#                 refund = MRP1 - 0.3*MRP1
             
 
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import threading
+
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -6,8 +8,8 @@ class BookingSystem:
     def __init__(self):
         self.booking_type = 0  # Default value for booking_type
         self.MRP1 = 10000  # Default value for MRP1
-        self.x = 9  # x:rating
         self.MRP2 = 0
+        self.x_semaphore = threading.Semaphore(10)
         self.refund = 0
 
         self.transaction_message = ""
@@ -23,37 +25,57 @@ class BookingSystem:
         self.add_page_message = "THIS IS AN ADVERTISMENT/SURVEY"
 
     def rebooking_overhead(self):
-        self.MRP2 = 0.1 * self.MRP1  # Fix the calculation here
+        self.MRP2 = 0.1 * self.MRP1  # Fix_semaphore the calculation here
         self.overhead_message = f"Booking price: {self.MRP1}"
         self.overhead_message += f"\nAdditional rebooking overhead: {self.MRP2}"
         self.total_amount_message = f"Please pay total amount: {self.MRP1 + self.MRP2}"
 
     def handle_booking(self):
         if self.booking_type == 0:
-            if self.x == 10:
+            if self.x_semaphore._value == 10:
                 self.collect_MRP1()
             else:
-                self.x = self.x + 1
-                self.rating_message = f"rating increased to: {self.x}"
+                self.x_semaphore.release()
+                self.rating_message = f"rating increased to: {self.x_semaphore._value}"
                 self.collect_MRP1()
         elif self.booking_type == 1:
-            if self.x == 10:
-                self.x = self.x - 1
-                self.rating_message = f"rating decreased to: {self.x}"
+            if self.x_semaphore._value == 10:
+                self.x_semaphore.acquire()
+                self.rating_message = f"rating decreased to: {self.x_semaphore._value}"
                 self.collect_MRP1()
             else:
-                if self.x < 5 and self.x > 0:
+                if self.x_semaphore._value < 5 and self.x_semaphore._value > 0:
                     self.call_add_page()
-                    self.x = self.x - 1
-                    self.rating_message = f"rating decreased to: {self.x}"
+                    self.x_semaphore = self.x_semaphore - 1
+                    self.rating_message = f"rating decreased to: {self.x_semaphore}"
                     self.rebooking_overhead()
                 else:
                     self.call_add_page()
-                    self.x = self.x - 1
-                    self.rating_message = f"rating decreased to: {self.x}"
+                    self.x_semaphore.acquire()
+                    self.rating_message = f"rating decreased to: {self.x_semaphore._value}"
                     self.total_amount_message = f"Total amount to be paid: {self.MRP1}"
+        else:
+            if self.x_semaphore._value == 10:  # Check if the semaphore count is 0.
+                self.x_semaphore.acquire()
+                print(f"rating decreased to: {self.x_semaphore._value}")
+                refund = self.MRP1 - 0.1 * self.MRP1
+                self.total_amount_message = f"Amount refunded : {refund}"        
+            else:
+                if self.x_semaphore._value < 5 and self.x_semaphore._value > 0:  # Check the semaphore count.
+                    self.call_add_page()
+                    self.x_semaphore.acquire()
+                    self.rating_message = f"rating decreased to: {self.x_semaphore._value}"
+                    refund = self.MRP1 - 0.2 * self.MRP1
+                    self.total_amount_message = f"Amount refunded : {refund}"     
+                else:
+                    self.call_add_page()
+                    self.x_semaphore.acquire()
+                    self.rating_message = f"rating decreased to: {self.x_semaphore._value}"
+                    refund = self.MRP1 - 0.3 * self.MRP1
+                    self.total_amount_message = f"Amount refunded : {refund}"     
 
 booking_system = BookingSystem()  # Initialize the booking_system object
+
 
 
 @app.route('/')
